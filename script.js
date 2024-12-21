09fff59c709fada2304dc2ab0a042114752bfc01
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-21 11:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function leapYear(year) {
+if(year%4==0 && year%100!=0 || year%400==0) return 366;
+	else return 365;
+
+}
+let year = Number(prompt());
+alert(leapYear(year))
